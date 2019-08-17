abd4d856fc7b411aa855db908689746d3feb88dd
Moomba Muloba <moomba.muloba@pexsoftware.com>
2019-08-17 13:26:38 +0100

    Add unit testing
    wip - add snapshot testing with react
    
    wip - add new files for snapshot testing
    
    wip - try testing with react-testing-library
    
    wip - try DOM testing with enzyme
    
    wip - update directory structure
    
    wip adding sass
    
    Add README

---
+++ README.md
@@ -0,0 +1,10 @@
+# React Boilerplate
+
+A react starter kit which includes tools required for unit tests, snapshot testing, live development reloading and styles.
+
+- React
+- Jest
+- Enzyme
+- Webpack
+- Hot Module replace
+- SASS
--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import {hot} from "react-hot-loader";
+import CheckboxWithLabel from './components/CheckboxWithLabel';
 import "./App.css";
+import "../assets/sass/global.scss";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> Hello, MOomba! </h1>
+        <CheckboxWithLabel labelOn="On" labelOff="Off" />
       </div>
     );
   }
